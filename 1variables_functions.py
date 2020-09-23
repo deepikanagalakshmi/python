c1b33ee1b923e2b8b49965b9f1d0edb9ec7953c8
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-09-23 11:42:06 +0530

    Create 1variables_functions.py

---
+++ 1variables_functions.py
@@ -0,0 +1,25 @@
+import turtle
+
+a = 5
+print(a)
+
+b = "hello"
+print(b)
+
+credit_card = 444446633211
+print(credit_card)
+
+my_turtle = turtle.Turtle()
+my_turtle.speed()
+
+# square
+def square():
+    my_turtle.forward(100)
+    my_turtle.right(90)
+    my_turtle.forward(100)
+    my_turtle.right(90)
+    my_turtle.forward(100)
+    my_turtle.right(90)
+    my_turtle.forward(100)
+
+square()
